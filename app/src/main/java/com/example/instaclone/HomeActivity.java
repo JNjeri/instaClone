f4a68b7355c7a9cb14dca0edf5cf1d3d8d91c5c0
JNjeri <tamiranjeri98@gmail.com>
2019-09-06 11:51:31 +0300

    first commit

---
+++ app/src/main/java/com/example/instaclone/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.instaclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.design.widget.CoordinatorLayout;
+//import android.support.design.widget.AppBarLayout;
+//import android.support.design.widget.CollapsingToolbarLayout;
+//import android.support.v7.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
